8553bf55da18f56c05243c9fbe12a9b62a654866
Bergan16 <74006739+Bergan16@users.noreply.github.com>
2021-03-17 12:02:42 +0100

    Js fil

---
+++ testbed.js
@@ -0,0 +1,9 @@
+//javascript
+function changeText(){
+	var rubrik=document.getElementById('rubrik');
+	rubrik.innerHTML = 'Poff.....';
+}
+
+function chBackcolor(color){
+ document.body.style.background= color;
+}
